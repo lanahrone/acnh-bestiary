2b2743b81ba51bc6512321e7451c35dfc7cdb527
lanahrone <lanahrone@gmail.com>
2022-05-11 19:15:32 +0200

    Add grids to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <button v-for="[gridType, grid] in Object.entries(grids)" :key="gridType" @click="currentGrid = grid">
+    {{ grid.label }}
+  </button>
+
+  <TheGrid v-if="currentGrid" :grid="currentGrid"></TheGrid>
 </template>
 
 <script>
+import insects from "./data/insects.json"
+import fishes from "./data/fishes.json"
+import TheGrid from "./components/TheGrid.vue"
+
 export default {
-  name: 'App',
-  components: {}
+  name: "App",
+  components: { TheGrid },
+  data() {
+    return {
+      currentGrid: null,
+      grids: {
+        insects: { width: 16, height: 5, label: "🦋", data: insects },
+        fishes: { width: 16, height: 5, label: "🐟", data: fishes },
+        deepsea: { width: 8, height: 5, label: "🐚", data: [] },
+      }
+    }
+  }
 }
 </script>
