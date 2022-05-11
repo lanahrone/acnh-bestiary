4f1d7fd2591718a18a06ad301eccfa7bb0daf657
lanahrone <lanahrone@gmail.com>
2022-05-11 19:15:33 +0200

    Display basic grid

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -1,10 +1,31 @@
 <template>
-    TheGrid
+    <table border="1">
+        <tr v-for="(r, rIndex) in Array(grid.height)" :key="`row${rIndex}`">
+            <td v-for="(c, cIndex) in Array(grid.width)" :key="`cell${cIndex}`">
+                {{ items[rIndex][cIndex] }}
+            </td>
+        </tr>
+    </table>
 </template>
 
 <script>
 export default {
     name: "TheGrid",
-    props: { grid: Object }
+    props: { grid: Object },
+    computed: {
+        items() {
+            const items = {}
+            ;[...this.grid.data].forEach((item) => {
+                const x = item.coords[0] - 1
+                const y = item.coords[1] - 1
+                if (items[x]) {
+                    items[x][y] = item
+                } else {
+                    items[x] = { [y]: item }
+                }
+            })
+            return items
+        }
+    }
 }
 </script>
